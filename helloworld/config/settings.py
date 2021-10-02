c9d87531f460c490650ae67f9ce6281af7ce18d5
Filip Przychodni <pierwiastekz3@interia.pl>
2021-10-02 14:28:42 +0200

    Updates for heroku deployment

--- helloworld/config/settings.py
+++ helloworld/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b%v2=3k84ks!%-)d^x^jdxih=vj$sl^0s*zc5dn6pz#m6f30n@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
