ac7f67b7f08fdcbc43043907cd4a24831a3b46c3
Filip Przychodni <pierwiastekz3@interia.pl>
2021-10-02 18:34:34 +0200

    Updates for Heroku

--- Message Board/config/settings.py
+++ Message Board/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vm+_d1+-)xmbp6i8b61n2w-+p0pof8z((o$plhyp62xaoqk_l$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
